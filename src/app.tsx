e46220e9899aca788b29c05bc90d9c67134e3e72
Gabriel <132632103+bielaugustos@users.noreply.github.com>
2024-04-02 17:53:16 +0000

    docs(react) como criar componentes reacct

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+// componentes são elementos construidos com html - exemplo
+
+function Meubotao() {
+  return <button>meu botão</button>
+}
+
+export function App() {
+  return(
+    <>
+    <Meubotao />
+    <Meubotao />
+    <Meubotao />
+    <Meubotao />
+    <Meubotao />
+    <Meubotao />
+    </>
+    ) 
+}
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import { App } from './app'
 import './index.css'
+
+// react funciona como um agregador de componentes e propriedades
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
