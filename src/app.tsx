3e8c1a18cc9a18ba90b5b890b5cbb2245ffe1857
Gabriel <132632103+bielaugustos@users.noreply.github.com>
2024-04-02 22:37:07 +0000

    docs(rtpescript) como trabalhar com propriedades, tipar variaveis e estaplicar estilos

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,21 @@
 // componentes são elementos construidos com html - exemplo
+// para utilizar propriedades e estilos é necessário colocar chaves {} nos elementos que serão utilizados como variaveis
+// quando se utiliza typescript é preciso determinar o formato dos parametros que a função recebe (tipagem da variavel)
+interface MeuBotaoProps {
+  texto: string;
+}
 
-function Meubotao() {
-  return <button>meu botão</button>
+function Meubotao(props: MeuBotaoProps) {
+
+return <button className="button">{props.texto}</button>
 }
 
 export function App() {
   return(
     <>
-    <Meubotao />
-    <Meubotao />
-    <Meubotao />
-    <Meubotao />
-    <Meubotao />
-    <Meubotao />
+    <Meubotao texto="Clique aqui"/>
+    <Meubotao texto="Botão 2"/>
+    <Meubotao texto="Botão 3"/>
     </>
     ) 
 }
